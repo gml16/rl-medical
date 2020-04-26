814156265b07dcb3457a46d33a94dc352b688b57
Guy Leroy <g.m.leroy@outlook.com>
2020-04-26 18:24:40 +0100

    Adding test skeleton

---
+++ examples/LandmarkDetection/DQN/tests/test_expreplay.py
@@ -0,0 +1,18 @@
+from ..expreplay import ReplayMemory
+
+def test_instantiate_expreplay():
+    replay = ReplayMemory(max_size=10,
+                          state_shape=(3,3),
+                          history_len=4,
+                          agents=1)
+    assert replay._curr_pos == 0
+    assert replay._curr_size == 0
+    assert not replay.state.any()
+    assert not replay.action.any()
+    assert not replay.reward.any()
+    assert not replay.isOver.any()
+    assert replay.state.shape == (1, 10, 3, 3)
+    assert replay.action.shape == (1, 10)
+    assert replay.reward.shape == (1, 10)
+    assert replay.isOver.shape == (1, 10)
+    assert len(replay._hist) == 0
